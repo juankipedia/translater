97465ad2371397b66aa9470d61d5fa4e7dfbc431
juandiego <juandiegp17@gmail.com>
2017-05-30 23:02:16 -0400

    Modification of Markdown objects rules, and also code were comment with a doxygen style.
    Modifications on Markdown objects were done to make
    Markdown objects documents more readable and natural

--- src/addressBook.md
+++ src/addressBook.md
@@ -1,5 +1,3 @@
-parsed: Map("address book" -> Map("name" -> "john smith", "address" -> Map("street" -> "10 Market street", "city" -> "San Francisco, CA", "zip" -> 94111.0), "phone numbers" -> List("04140802832", "04149798710")), "value" -> 333.0)
-parsed: Map("address book" -> Map("name" -> "john smith", "address" -> Map("street" -> "10 Market street", "city" -> "San Francisco, CA", "zip" -> 94111.0), "phone numbers" -> List("04140802832", "04149798710")), "value" -> 333.0)
 # ObjectDefinition
   ## "address book":
        # ObjectDefinition
@@ -13,12 +11,10 @@ parsed: Map("address book" -> Map("name" -> "john smith", "address" -> Map("stre
                     "San Francisco, CA".
                   ## "zip":
                     94111
-                |
-            .
+                # EndObjectDefinition .
             ## "phone numbers":
             *"04140802832",
              "04149798710"*
-      |
-  .
+      # EndObjectDefinition .
   ## "value": 333
-|
+# EndObjectDefinition
--- src/main/scala/MarkdownParser/Markdown.scala
+++ src/main/scala/MarkdownParser/Markdown.scala
@@ -1,36 +1,42 @@
-package MarkdownParser
-import java.io.FileReader
-
-import scala.util.parsing.combinator._
-import java.io.FileReader._
 /**
-  * Created by Juan Diego on 5/30/2017.
+  *file: Markdown.scala
+  *
+  *brief: Parser for objects formatted with Markdown, Markdown text should has some
+          rules:
+            -Objects are members separated by "." and that are among expressions
+             " # ObjectDefinition " and " # EndObjectDefinition "
+            -Arrays are values separated by "," and that are among "*"
+            -Members are Strings and values separated by ":", and begins with "##"
+            -Values are Objects or Arrays, Strings, Floating point number,"null","true"
+             or "false"
+  *
+  *version 1.0.
+  *date 30/05/2017
   */
 
-class JSON extends JavaTokenParsers {
-  def obj: Parser[Map[String, Any]] = "{"~>repsep(member, ",")<~"}" ^^ {Map() ++ _}
-  def arr: Parser[List[Any]] = "["~>repsep(value,",")<~"]"
-  def member: Parser[(String,Any)] = stringLiteral~":"~value ^^ {case name~":"~value => (name,value)}
-  def value: Parser[Any] = (
-      obj
-      | arr
-      | stringLiteral
-      | floatingPointNumber ^^ (_.toDouble)
-      | "null" ^^ (x => null)
-      | "true" ^^ (x => true)
-      | "false" ^^ (x => false)
-    )
-}
+//Package definitions and imports
+package MarkdownParser
+import java.io.FileReader
+import scala.util.parsing.combinator._
 
+/**
+  *brief: Main Program to prove Markdown Parser
+  *params: strings arguments of program
+  *date 30/05/2017
+  */
 object ParseJSON extends Markdown {
   def main(args: Array[String]){
     //  render = new FileReader(args(0))
-    println(parseAll(value,"# ObjectDefinition\n  ## \"address book\":\n       # ObjectDefinition\n            ## \"name\":\n                \"john smith\".\n            ## \"address\":\n                # ObjectDefinition\n                  ## \"street\":\n                    \"10 Market street\".\n                  ## \"city\":\n                    \"San Francisco, CA\".\n                  ## \"zip\":\n                    94111\n                |\n            .\n            ## \"phone numbers\":\n            *\"04140802832\",\n             \"04149798710\"*\n      |\n  .\n  ## \"value\": 333\n|"))
+    println(parseAll(value,"# ObjectDefinition\n  ## \"address book\":\n       # ObjectDefinition\n            ## \"name\":\n                \"john smith\".\n            ## \"address\":\n                # ObjectDefinition\n                  ## \"street\":\n                    \"10 Market street\".\n                  ## \"city\":\n                    \"San Francisco, CA\".\n                  ## \"zip\":\n                    94111\n                # EndObjectDefinition .\n            ## \"phone numbers\":\n            *\"04140802832\",\n             \"04149798710\"*\n      # EndObjectDefinition .\n  ## \"value\": 333\n# EndObjectDefinition"))
   }
 }
 
+/**
+  *brief: Markdown parser class that extends from JavaTokenParsers
+  *date 30/05/2017
+  */
 class Markdown extends JavaTokenParsers{
-  def obj: Parser[Map[String, Any]] = "# ObjectDefinition"~>repsep(member, ".")<~"|" ^^ {Map() ++ _}
+  def obj: Parser[Map[String, Any]] = "# ObjectDefinition"~>repsep(member, ".")<~"# EndObjectDefinition" ^^ {Map() ++ _}
   def arr: Parser[List[Any]] = "*"~>repsep(value,",")<~"*"
   def member: Parser[(String,Any)] = "##"~>stringLiteral~":"~value ^^ {case name~":"~value => (name,value)}
   def value: Parser[Any] = (
