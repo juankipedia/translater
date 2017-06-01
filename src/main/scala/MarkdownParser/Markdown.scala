1b426a46ec2d1341d61e52f6e68a9693e51c97a3
juandiego <juandiegp17@gmail.com>
2017-06-01 17:42:20 -0400

    Translation is almost done.
    JSON protocol was implemented and is working however it has some
    problems.

--- src/main/scala/MarkdownParser/Markdown.scala
+++ src/main/scala/MarkdownParser/Markdown.scala
@@ -23,10 +23,36 @@ import scala.util.parsing.combinator._
   *params: strings arguments of program
   *date 30/05/2017
   */
+
+import spray.json._
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit object MapJsonFormat extends JsonFormat[Map[String, Any]] {
+    def write(m: Map[String, Any]) = {
+      JsObject(m.mapValues {
+        case v: String => JsString(v)
+        case v: Double => JsNumber(v)
+        case v: Map[String, Any] => write(v)
+        case v: List[Any] => JsString("[" + listToJsonArray(v) + "]")
+        case v: Any => JsString(v.toString)
+      })
+    }
+    def read(value: JsValue) = ???
+    def listToJsonArray (v: List[Any]) : String = {
+      if(v.tail.isEmpty)
+        v.head.toString
+      else
+        v.head.toString + "," + listToJsonArray(v.tail)
+    }
+  }
+}
+import spray.json._
+import MyJsonProtocol._
+
 object ParseMarkdown extends Markdown{
   def main(args: Array[String]){
     val map = parseMarkdown("\\Users\\Juan Diego\\Documents\\MAMMUT\\translater\\src\\main\\scala\\MarkdownParser\\addressBook.md")
     println(map.apply("\"address book\""))
+    println(map.toJson)
   }
 }
 /**
