c6c70fd7b1d4b9bc62c72d89ae6ee04907f0293d
Marco Alejandro <kada9001@gmail.com>
2017-06-02 05:36:55 -0400

    test

--- src/test/scala/ParseMarkdownTester.scala
+++ src/test/scala/ParseMarkdownTester.scala
@@ -5,37 +5,38 @@
 import org.scalatest.WordSpec
 import MarkdownTranslater._
 /**
-  * brief: Testing the parse from a Markdown JSON to an
-  *   scala Map[String, Any]
+  * brief: Testing the parse from a Markdown to an
+  *   scala Map[String, Any] to an JSON
   */
 class ParseMarkdownSpec extends WordSpec{
 
   "ParseMarkdown " must {
 
-    "Parse the addresBook.md file" in {
+    "Parse the addressBook.md file to an Map" in {
         /*Linux*/
-        val myMap = ParseMarkdown.onlyMarkdown("./src/main/scala/MarkdownTranslater/addressBook.md")
+        ParseMarkdown.onlyMarkdown("./src/main/scala/MarkdownTranslater/addressBook.md")
         /*Windows*/
-        //val myMap = ParseMarkdown.main(".\\src\\main\\scala\\MarkdownTranslater\\addresBook.md")
+        //ParseMarkdown.main(".\\src\\main\\scala\\MarkdownTranslater\\addressBook.md")
     }
 
     
     "Fail to read an un-format Markdown file" in {
       /*Linux*/
       intercept[Exception] {
-        val myMap = ParseMarkdown.onlyMarkdown("./src/main/scala/MarkdownTranslater/addressBookWrong.md")
+        ParseMarkdown.onlyMarkdown("./src/main/scala/MarkdownTranslater/addressBookWrong.md")
         /*Windows*/
-        //val myMap = ParseMarkdown.main(".\\src\\main\\scala\\MarkdownTranslater\\addresBookWrong.md")
+        //ParseMarkdown.main(".\\src\\main\\scala\\MarkdownTranslater\\addressBookWrong.md")
       }
+    }
+
+    "Parse the addressBook.md file to a JSON usin SprayJSON" in{
+      /*Linux*/
+      ParseMarkdown.markdownToJson("./src/main/scala/MarkdownTranslater/addressBook.md")
+      /*Windows*/
+      //ParseMarkdown.markdownToJson(".\\src\\main\\scala\\MarkdownTranslater\\addressBook.md")
     }
 
   }
 
 }
 
-/*
-  *brief: testing the parse from a Markdown-JSON to a final JSON object
- */
-class ParseMarkdownToJsonSpec extends WordSpec{
-
-}
